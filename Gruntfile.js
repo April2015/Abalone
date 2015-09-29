e6a1eefdfd744837cb4199f019e34564fe2d874e
April2015 <yanhongoy@gmail.com>
2015-09-28 23:09:27 -0400

    reset single run

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     karma: {
       unit: {
         configFile: 'karma.conf.js',
-        singleRun: false
+        singleRun: true
       }
     },
     concat: {
